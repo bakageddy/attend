fd212d35fb884363d6508de9e08b9a161037aef6
Dinesh Kumar <gdineshda@gmail.com>
2025-04-15 17:40:45 +0530

    UI: try handlebars.js

---
+++ app/src/main/ui/templates/student.handlebar.js
@@ -0,0 +1,10 @@
+<script id="idempotent__student" type="text/x-handlebars-template">
+	<div class="student_element">
+		<div class="student_id">
+			{{ rollNo }}
+		</div>
+		<div class="student_name">
+			{{ name }}
+		</div>
+	</div>
+</script>
