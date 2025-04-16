930b1eeedbe0797e9baa2b8d186bf977594a355c
Dinesh Kumar <gdineshda@gmail.com>
2025-04-16 12:37:00 +0530

    UI: I don't want handlebars

--- app/src/main/ui/js/student.js
+++ app/src/main/ui/js/student.js
@@ -1,5 +1,3 @@
-import { compile } from "./handlebars";
-
 let student_result_handle = document.getElementById("result_display");
 let student_id_search = document.getElementById("search__student__id");
 let student_name_search = document.getElementById("search__student__name");
@@ -19,7 +17,7 @@ student_id_search.addEventListener('input', async (event) => {
 
 	// Reset the result div
 	student_result_handle.innerHTML = '';
-	student_result_handle.appendChild(render_student_id(results_json));
+	student_result_handle.innerHTML = render_student_id(results_json);
 })
 
 student_name_search.addEventListener('input', async (event) => {
@@ -29,7 +27,7 @@ student_name_search.addEventListener('input', async (event) => {
 	}
 
 	let url_params = new URLSearchParams(
-		{ pattern: `${search_string}%` }
+		{ pattern: search_string }
 	).toString();
 	let search_results = await fetch("/app/api/student/search?" + url_params, { method: "GET" });
 	let results_json = await search_results.json();
@@ -37,12 +35,10 @@ student_name_search.addEventListener('input', async (event) => {
 	student_result_handle.innerHTML = '';
 	results_json.map(student => {
 		console.log(student);
-		result.appendChild(render_student_id(student));
+		student_result_handle.innerHTML += render_student_id(student);
 	});
 })
 
 const render_student_id = (json_data) => {
-	let student_template = document.getElementById("idempotent__student").innerHTML;
-	let template = compile(student_template);
-	return template(json_data);
+	return `<div class="student__element">${json_data.rollNo} : ${json_data.name}</div>`
 }
