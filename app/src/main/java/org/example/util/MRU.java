eb69fabfd510ec281765d5ca0d2afe94c4b3c3b2
Dinesh Kumar <gdineshda@gmail.com>
2025-05-07 13:26:31 +0530

    API: fix bug updating cache

--- app/src/main/java/org/example/util/MRU.java
+++ app/src/main/java/org/example/util/MRU.java
@@ -4,11 +4,15 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
+import com.google.errorprone.annotations.ThreadSafe;
+import com.google.errorprone.annotations.concurrent.GuardedBy;
+
 // oldest <-> node(s) <-> latest
 // remove from latest
 // add to latest
 
-// TODO: Implement Lockless Caching
+// TODO: Implement Lockless Caching / A more concurrent cache
+@ThreadSafe
 public class MRU<Key, Value> implements Cache<Key, Value> {
 	private long size;
 	private Map<Key, Node<Key, Value>> cache;
@@ -30,6 +34,15 @@ public class MRU<Key, Value> implements Cache<Key, Value> {
 		return cache.toString();
 	}
 
+	public synchronized long cache_size() {
+		return this.cache.size();
+	}
+
+	public synchronized long cache_capacity() {
+		return this.size;
+	}
+
+	@GuardedBy("this")
 	public synchronized void purge() {
 		for (var item : this.cache.values()) {
 			if (item.is_stale()) {
@@ -39,7 +52,16 @@ public class MRU<Key, Value> implements Cache<Key, Value> {
 		}
 	}
 
+	@GuardedBy("this")
+	public synchronized void flush() {
+		for (var item : this.cache.values()) {
+			Node.remove_self(item);
+			this.cache.remove(item.key);
+		}
+	}
+
 	@Override
+	@GuardedBy("this")
 	public synchronized Optional<Value> get(Key key) {
 		if (!cache.containsKey(key)) {
 			return Optional.empty();
@@ -58,20 +80,22 @@ public class MRU<Key, Value> implements Cache<Key, Value> {
 	}
 
 	@Override
+	@GuardedBy("this")
 	public synchronized void put(Key key, Value val) {
 		if (cache.containsKey(key)) {
 			Node<Key, Value> node = cache.get(key);
 			Node.remove_self(node);
 		}
 
-		Node<Key, Value> new_value = new Node<>(key, val);
-		cache.put(key, new_value);
-		Node.insert_before(new_value, latest);
-
-		if (cache.size() > size) {
+		if (cache.size() == size) {
 			Node<Key, Value> mru = latest.prev;
 			Node.remove_self(mru);
 			cache.remove(mru.key);
 		}
+
+		// Create new object to reset TTL
+		Node<Key, Value> new_value = new Node<>(key, val);
+		cache.put(key, new_value);
+		Node.insert_before(new_value, latest);
 	}
 }
