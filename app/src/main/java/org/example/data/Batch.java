5d5416e5699cd998e45857e25d588fc4ea4abd38
Dinesh Kumar <gdineshda@gmail.com>
2025-05-09 18:01:15 +0530

    REFACTOR(API): Batch & BatchData

---
+++ app/src/main/java/org/example/data/Batch.java
@@ -0,0 +1,206 @@
+package org.example.data;
+
+import java.util.Optional;
+
+import org.example.util.LRU;
+import org.example.util.Result;
+import org.example.util.Validator;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Batch
+ */
+public class Batch {
+	private static LRU<Long, List<Batch>> batch_owner_cache = null;
+	private static LRU<String, List<Batch>> batch_pattern_cache = null;
+
+	public static void set_owner_cache(LRU<Long, List<Batch>> cache) {
+		if (Batch.batch_owner_cache != null) {
+			Batch.batch_owner_cache.flush();
+		}
+		Batch.batch_owner_cache = cache;
+	}
+
+	public static void set_pattern_cache(LRU<String, List<Batch>> cache) {
+		if (Batch.batch_pattern_cache != null) {
+			Batch.batch_pattern_cache.flush();
+		}
+		Batch.batch_pattern_cache = cache;
+	}
+
+	public static void delete_owner_cache() {
+		Batch.batch_owner_cache.flush();
+	}
+
+	public static void delete_pattern_cache() {
+		Batch.batch_pattern_cache.flush();
+	}
+
+	public static Result<Batch, String> search(long batchid) {
+		Optional<Connection> optional_cnx = Database.get_connection();
+		if (optional_cnx.isEmpty()) {
+			return Result.err("Failed to acquire connection");
+		}
+		try (
+			Connection cnx = optional_cnx.get()
+		) {
+			PreparedStatement stmt = cnx.prepareStatement(
+				"SELECT BatchID, TeacherID, Name FROM Batch WHERE BatchID = ?;"
+			);
+			stmt.setLong(1, batchid);
+			ResultSet rst = stmt.executeQuery();
+
+			if (!rst.next()) {
+				return Result.err("No Batch found with the given ID");
+			}
+
+			long teacherid = rst.getLong(2);
+			String name = rst.getString(3);
+			return Result.ok(new Batch(batchid, teacherid, name));
+		} catch (Exception e) {
+			return Result.err(e.getMessage());
+		}
+	}
+
+	public static Result<List<Batch>, String> search(String pattern) {
+		Optional<String> validated_pattern = Validator.validate_sql(pattern);
+		if (validated_pattern.isEmpty()) {
+			return Result.err("Expect Valid SQL.");
+		}
+
+		Optional<Connection> optional_cnx = Database.get_connection();
+		if (optional_cnx.isEmpty()) {
+			return Result.err("Failed to acquire connection");
+		}
+
+		try (
+			Connection cnx = optional_cnx.get();
+		) {
+			PreparedStatement exists = cnx.prepareStatement(
+				"SELECT 1 FROM Batch WHERE Name LIKE ? LIMIT 1;"
+			);
+			exists.setString(1, validated_pattern.get());
+			ResultSet exists_rst = exists.executeQuery();
+
+			if (!exists_rst.next()) {
+				return Result.err("No Batch with the given pattern");
+			}
+
+			PreparedStatement stmt = cnx.prepareStatement(
+				"SELECT BatchID, TeacherID, Name FROM Batch WHERE Name LIKE ? LIMIT 20;"
+			);
+			stmt.setString(1, validated_pattern.get());
+
+			ResultSet rst = stmt.executeQuery();
+			List<Batch> batches = new ArrayList<>();
+			while (rst.next()) {
+				long batchid = rst.getLong(1);
+				long teacherid = rst.getLong(2);
+				String name = rst.getString(3);
+				batches.addLast(new Batch(batchid, teacherid, name));
+			}
+
+			return Result.ok(batches);
+		} catch (Exception e) {
+			return Result.err(e.getMessage());
+		}
+	}
+
+	public static Result<List<Batch>, String> search_teacherid(long teacherid) {
+		Optional<Connection> optional_cnx = Database.get_connection();
+		if (optional_cnx.isEmpty()) {
+			return Result.err("Failed to acquire connection");
+		}
+		try (
+			Connection cnx = optional_cnx.get()
+		) {
+			PreparedStatement stmt = cnx.prepareStatement(
+				"SELECT BatchID, Name FROM Batch WHERE TeacherID = ?;"
+			);
+			stmt.setLong(1, teacherid);
+			ResultSet rst = stmt.executeQuery();
+
+			List<Batch> batches = new ArrayList<>();
+			while (rst.next()) {
+				long batchid = rst.getLong(1);
+				String name = rst.getString(2);
+				batches.addLast(new Batch(batchid, teacherid, name));
+			}
+			return Result.ok(batches);
+		} catch (Exception e) {
+			return Result.err(e.getMessage());
+		}
+
+	}
+
+	public static Result<Long, String> create(long teacherid, String name) {
+		Optional<String> validated_pattern = Validator.validate_sql(name);
+		if (validated_pattern.isEmpty()) {
+			return Result.err("You shalln't try to hack me T-T");
+		}
+
+		Optional<Connection> optional_cnx = Database.get_connection();
+		if (optional_cnx.isEmpty()) {
+			return Result.err("Failed to acquire connection");
+		}
+
+		try (
+			Connection cnx = optional_cnx.get();
+		) {
+			PreparedStatement stmt = cnx.prepareStatement("INSERT INTO Batch(Name, TeacherID) VALUES(?, ?) RETURNING BatchID;");
+			stmt.setString(1, name);
+			stmt.setLong(2, teacherid);
+
+			ResultSet rst = stmt.executeQuery();
+			if (!rst.next()) {
+				return Result.err("Failed to create batch");
+			}
+
+			long batchid = rst.getLong(1);
+			return Result.ok(batchid);
+		} catch (Exception e) {
+			return Result.err(e.getMessage());
+		}
+	}
+
+	public static Result<Void, String> delete(long batchid, long teacherid) {
+		Optional<Connection> optional_cnx = Database.get_connection();
+		if (optional_cnx.isEmpty()) {
+			return Result.err("Failed to acquire connection");
+		}
+
+		try (
+			Connection cnx = optional_cnx.get();
+		) {
+			PreparedStatement stmt = cnx.prepareStatement(
+				"DELETE FROM Batch WHERE BatchID = ? AND TeacherID = ?;"
+			);
+			stmt.setLong(1, batchid);
+			stmt.setLong(2, teacherid);
+
+			int no = stmt.executeUpdate();
+			if (no != 1) {
+				return Result.err("Failed to delete batch");
+			}
+
+			return Result.ok(null);
+		} catch (Exception e) {
+			return Result.err(e.getMessage());
+		}
+	}
+
+	long batchid;
+	long teacherid;
+	String name;
+
+	public Batch(long batchid, long teacherid, String name) {
+		this.batchid = batchid;
+		this.teacherid = teacherid;
+		this.name = name;
+	}
+}
---
+++ app/src/main/java/org/example/data/BatchData.java
@@ -0,0 +1,216 @@
+package org.example.data;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.example.util.LRU;
+import org.example.util.Result;
+
+public class BatchData {
+	private static LRU<Long, ArrayList<Student>> cache;
+	public static void set_cache(LRU<Long, ArrayList<Student>> cache) {
+		if (BatchData.cache != null) {
+			BatchData.cache.flush();
+		}
+
+		BatchData.cache = cache;
+	}
+
+	public static void delete_cache() {
+		if (BatchData.cache == null) return;
+		BatchData.cache.flush();
+		BatchData.cache = null;
+	}
+
+	public static Optional<BatchData> search(long batchid) {
+		Optional<Connection> optional_cnx = Database.get_connection();
+		if (optional_cnx.isEmpty()) {
+			return Optional.empty();
+		}
+
+		try (
+			Connection cnx = optional_cnx.get();
+		) {
+			PreparedStatement stmt = cnx.prepareStatement(
+				"SELECT Teacher.TeacherID, Student.RollNo, Student.Name FROM BatchData JOIN Student ON BatchData.RollNo = Student.RollNo WHERE BatchData.BatchID = ?;"
+			);
+			stmt.setLong(1, batchid);
+			ResultSet rst = stmt.executeQuery();
+
+			Optional<Long> teacherid = Optional.empty();
+			List<Student> students = new ArrayList<>();
+			while (rst.next()) {
+				teacherid = Optional.of(rst.getLong(1));
+				long rollno = rst.getLong(2);
+				String name = rst.getString(3);
+
+				students.addLast(new Student(rollno, name));
+			}
+
+			if (teacherid.isEmpty()) {
+				students.clear();
+				return Optional.empty();
+			}
+			return Optional.of(new BatchData(batchid, teacherid.get(), students));
+
+		} catch (Exception e) {
+			return Optional.empty();
+		}
+
+	}
+
+	public static Result<Void, String> add(long batchid, long rollno) {
+		Optional<Connection> optional_cnx = Database.get_connection();
+		if (optional_cnx.isEmpty()) {
+			return Result.err("Failed to acquire connection");
+		}
+
+		try (
+			Connection cnx = optional_cnx.get();
+		) {
+			PreparedStatement stmt = cnx.prepareStatement(
+				"INSERT INTO BatchData(batchid, rollno) VALUES(?, ?);"
+			);
+			stmt.setLong(1, batchid);
+			stmt.setLong(2, rollno);
+
+			int no_of_rows = stmt.executeUpdate();
+			if (no_of_rows != 1) {
+				return Result.err("BEEP BOOP, Insert failed");
+			}
+
+			return Result.ok(null);
+		} catch (Exception e) {
+			return Result.err(e.getMessage());
+		}
+	}
+
+	public static Result<Void, String> add(long batchid, long[] rollnos) {
+		Optional<Connection> optional_cnx = Database.get_connection();
+		if (optional_cnx.isEmpty()) {
+			return Result.err("Failed to acquire connection");
+		}
+
+		try (
+			Connection cnx = optional_cnx.get();
+		) {
+			String query = construct_query(batchid, rollnos);
+			PreparedStatement stmt = cnx.prepareStatement(
+				query
+			);
+
+			int no_of_rows = stmt.executeUpdate();
+			if (no_of_rows != rollnos.length) {
+				return Result.err("BEEP BOOP, failed to add students");
+			}
+
+			return Result.ok(null);
+		} catch (Exception e) {
+			return Result.err(e.getMessage());
+		}
+	}
+
+	private static String construct_query(long batchid, long[] rollnos) {
+		StringBuilder s = new StringBuilder();
+		s.append("INSERT INTO BatchData(batchid, rollno) VALUES");
+		for (int i = 0; i < rollnos.length; i++) {
+			if (i != 0) s.append(",");
+
+			s.append("(")
+				.append(batchid)
+				.append(",")
+				.append(rollnos[i])
+				.append(")");
+		}
+		s.append(";");
+		return s.toString();
+	}
+
+	public static Result<Void, String> delete(long batchid, long rollno) {
+		Optional<Connection> optional_cnx = Database.get_connection();
+		try (
+			Connection cnx = optional_cnx.get();
+		) {
+			PreparedStatement stmt = cnx.prepareStatement("DELETE FROM BatchData WHERE BatchID = ? AND RollNo = ?;");
+			stmt.setLong(1, batchid);
+			stmt.setLong(2, rollno);
+
+			int no = stmt.executeUpdate();
+			if (no != 1) {
+				return Result.err("BEEP BOOP, cannot delete student from batch");
+			}
+
+			return Result.ok(null);
+		} catch (Exception e) {
+			return Result.err(e.getMessage());
+		}
+	}
+
+	public static Result<Void, String> delete(long batchid, long rollnos[]) {
+		Optional<Connection> optional_cnx = Database.get_connection();
+		try (
+			Connection cnx = optional_cnx.get();
+		) {
+			String query = construct_delete_query(batchid, rollnos);
+			PreparedStatement stmt = cnx.prepareStatement(query);
+
+			int no = stmt.executeUpdate();
+			if (no != rollnos.length) {
+				return Result.err("BEEP BOOP, cannot delete student from batch");
+			}
+
+			return Result.ok(null);
+		} catch (Exception e) {
+			return Result.err(e.getMessage());
+		}
+	}
+
+	private static String construct_delete_query(long batchid, long[] rollnos) {
+		StringBuilder s = new StringBuilder();
+		s.append("DELETE FROM BatchData WHERE batchid=")
+			.append(batchid)
+			.append(" AND (");
+		for (int i = 0; i < rollnos.length; i++) {
+			if (i != 0) s.append(" OR ");
+			s.append("rollno=")
+				.append(rollnos[i]);
+		}
+		s.append(");");
+		return s.toString();
+	}
+
+	public static Result<Void, String> delete_all(long batchid) {
+		Optional<Connection> optional_cnx = Database.get_connection();
+		try (
+			Connection cnx = optional_cnx.get();
+		) {
+			PreparedStatement stmt = cnx.prepareStatement("DELETE FROM BatchData WHERE BatchID = ?;");
+			stmt.setLong(1, batchid);
+
+			stmt.executeUpdate();
+			return Result.ok(null);
+		} catch (Exception e) {
+			return Result.err(e.getMessage());
+		}
+	}
+
+	public long batchid;
+	public long teacherid;
+	public List<Student> students = null;
+
+	public BatchData(long batchid, long teacherid) {
+		this.batchid = batchid;
+		this.teacherid = teacherid;
+		this.students = null;
+	}
+
+	public BatchData(long batchid, long teacherid, List<Student> students) {
+		this.batchid = batchid;
+		this.teacherid = teacherid;
+		this.students = students;
+	}
+}
