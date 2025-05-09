74384c57a65e54ffbb3b83ac9a8c48a3a4657231
Dinesh Kumar <gdineshda@gmail.com>
2025-05-09 14:20:07 +0530

    API: update Context, remove hikari and add cache

--- app/src/main/java/org/example/Context.java
+++ app/src/main/java/org/example/Context.java
@@ -1,70 +1,49 @@
 package org.example;
 
-import org.example.util.LRU;
-import org.example.util.Cache;
+import java.util.List;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.pool.HikariPool;
+import org.example.data.Database;
+import org.example.data.Student;
+import org.example.data.Teacher;
+import org.example.util.Cache;
+import org.example.util.LRU;
 
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 
-import java.util.List;
-
 @WebListener
 public class Context implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		try {
-			Class.forName("org.postgresql.Driver");
-			String password = System.getenv("PGSQL_DATABASE_PASSWORD");
-			if (password == null) {
-				System.exit(1);
-			}
+		// NOTE: Research whether student_id_cache is important or not...
+		// TODO: Must refactor to use Student, Teacher, Subject :(
+		
+		Student.set_cache(new LRU<String, List<Student>>(30));
+		Teacher.set_cache(new LRU<String, List<Teacher>>(30));
+		Cache<String, String> subject_pattern_cache = new LRU<>(30);
+		Cache<Long, List<Student>> batch_id_cache = new LRU<>(30);
 
-			HikariConfig hconfig = new HikariConfig();
-			hconfig.setJdbcUrl("jdbc:postgresql://localhost/college");
-			hconfig.setUsername("postgres");
-			hconfig.setPassword(password);
-			HikariPool cpool = new HikariPool(hconfig);
+		// ctx.setAttribute("cnx_pool", cpool);
 
-			// NOTE: Research whether student_id_cache is important or not...
-			// TODO: Must refactor to use Student, Teacher, Subject :(
-			Cache<String, String> student_pattern_cache = new LRU<>(30);
-			// LRU<Long, String> student_id_cache = new LRU<>(30);
-
-			Cache<String, String> teacher_pattern_cache = new LRU<>(30);
-			// LRU<Long, String> teacher_id_cache = new LRU<>(30);
-
-			Cache<String, String> subject_pattern_cache = new LRU<>(30);
-			// LRU<Long, String> subject_id_cache = new LRU<>(30);
-			
-			Cache<Long, List<Student>> batch_id_cache = new LRU<>(30);
-
-			var ctx = sce.getServletContext();
-			ctx.setAttribute("cnx_pool", cpool);
-
-			ctx.setAttribute("student_pattern_cache", student_pattern_cache);
-			// ctx.setAttribute("student_id_cache", student_id_cache);
-
-			ctx.setAttribute("teacher_pattern_cache", teacher_pattern_cache);
-			// ctx.setAttribute("teacher_id_cache", teacher_id_cache);
-
-			ctx.setAttribute("subject_pattern_cache", subject_pattern_cache);
-			// ctx.setAttribute("subject_id_cache", subject_id_cache);
-			
-			ctx.setAttribute("batch_id_cache", batch_id_cache);
-		} catch (ClassNotFoundException e) {
-			System.err.println(e.getMessage());
-		}
+		// ctx.setAttribute("student_pattern_cache", student_pattern_cache);
+		// // ctx.setAttribute("student_id_cache", student_id_cache);
+		//
+		// ctx.setAttribute("teacher_pattern_cache", teacher_pattern_cache);
+		// // ctx.setAttribute("teacher_id_cache", teacher_id_cache);
+		//
+		// ctx.setAttribute("subject_pattern_cache", subject_pattern_cache);
+		// // ctx.setAttribute("subject_id_cache", subject_id_cache);
+		// 
+		// ctx.setAttribute("batch_id_cache", batch_id_cache);
 	}
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
 		
 		try {
-			HikariPool pool = (HikariPool) sce.getServletContext().getAttribute("cnx_pool");
-			pool.shutdown();
+			Database.close();
+			Student.delete_cache();
+			Teacher.delete_cache();
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
