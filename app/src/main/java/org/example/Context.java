e6f93cd94e53d87bca65339ef1d8d886f9ba54d1
Dinesh Kumar <gdineshda@gmail.com>
2025-05-09 17:59:54 +0530

    REFACTOR(API): cache setup

--- app/src/main/java/org/example/Context.java
+++ app/src/main/java/org/example/Context.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.example.data.Database;
 import org.example.data.Student;
+import org.example.data.Subject;
 import org.example.data.Teacher;
+import org.example.data.Batch;
+import org.example.data.BatchData;
 import org.example.util.Cache;
 import org.example.util.LRU;
 
@@ -16,34 +19,33 @@ import jakarta.servlet.annotation.WebListener;
 public class Context implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		// NOTE: Research whether student_id_cache is important or not...
-		// TODO: Must refactor to use Student, Teacher, Subject :(
-		
-		Student.set_cache(new LRU<String, List<Student>>(30));
-		Teacher.set_cache(new LRU<String, List<Teacher>>(30));
-		Cache<String, String> subject_pattern_cache = new LRU<>(30);
-		Cache<Long, List<Student>> batch_id_cache = new LRU<>(30);
+		Student.set_cache(new LRU<>(30));
+		Teacher.set_cache(new LRU<>(30));
 
-		// ctx.setAttribute("cnx_pool", cpool);
+		Subject.set_name_cache(new LRU<>(30));
+		Subject.set_code_cache(new LRU<>(30));
 
-		// ctx.setAttribute("student_pattern_cache", student_pattern_cache);
-		// // ctx.setAttribute("student_id_cache", student_id_cache);
-		//
-		// ctx.setAttribute("teacher_pattern_cache", teacher_pattern_cache);
-		// // ctx.setAttribute("teacher_id_cache", teacher_id_cache);
-		//
-		// ctx.setAttribute("subject_pattern_cache", subject_pattern_cache);
-		// // ctx.setAttribute("subject_id_cache", subject_id_cache);
-		// 
-		// ctx.setAttribute("batch_id_cache", batch_id_cache);
+		Batch.set_owner_cache(new LRU<>(30));
+		Batch.set_pattern_cache(new LRU<>(30));
+
+		BatchData.set_cache(new LRU<>(30));
 	}
+
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
-		
 		try {
 			Database.close();
+
 			Student.delete_cache();
 			Teacher.delete_cache();
+
+			Subject.delete_name_cache();
+			Subject.delete_code_cache();
+
+			Batch.delete_owner_cache();
+			Batch.delete_pattern_cache();
+
+			BatchData.delete_cache();
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
