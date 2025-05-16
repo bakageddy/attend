83feefc8deb98e075fe71dd982aeec252102610d
Dinesh Kumar <gdineshda@gmail.com>
2025-05-16 12:19:39 +0530

    REFACTOR(API): Extractor API, ErrKind -> TeacherSearch

--- app/src/main/java/org/example/api/TeacherSearch.java
+++ app/src/main/java/org/example/api/TeacherSearch.java
@@ -1,12 +1,11 @@
 package org.example.api;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-import java.util.Optional;
+import java.io.Writer;
 
 import org.example.data.Teacher;
-import org.example.util.Parser;
+import org.example.types.Err;
+import org.example.types.extractors.TeacherSearchRequest;
 import org.example.util.Result;
 import org.example.util.Serializer;
 
@@ -18,83 +17,50 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/api/teacher/search")
 public class TeacherSearch extends HttpServlet {
+	private static Result<Void, Err> search_and_serialize_to(TeacherSearchRequest req, Writer dst) {
+		if (req.getPattern() != null) {
+			return Teacher.search(req.getPattern())
+					.and_then(res -> Serializer.serialize_to(res, dst));
+		} else {
+			return Teacher.search(req.getTeacherid())
+					.and_then(res -> Serializer.serialize_to(res, dst));
+		}
+	}
+
+	private static int err_to_status(Err e) {
+		switch (e.kind) {
+			case ElementNotFound:
+				return HttpServletResponse.SC_NO_CONTENT;
+			case OutOfMemory:
+			case ClassNotFound:
+			case IllegalState:
+			case DBTimeout:
+			case JsonIOError:
+			case JsonSerializeError:
+				return HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+			default:
+				return HttpServletResponse.SC_BAD_REQUEST;
+		}
+	}
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String pattern_param = req.getParameter("pattern");
-		if (pattern_param != null) {
-			Result<List<Teacher>, String> result = Teacher.search(pattern_param);
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+		throws ServletException, IOException {
+		try (
+			Writer out = resp.getWriter();
+		){
+			Result<Void, Err> result = Teacher.extract(req.getParameterMap())
+				.and_then(search_request -> search_and_serialize_to(search_request, out));
+
 			if (result.isErr()) {
-				String err = result.err_msg();
-				if (err.equals("No results")) {
-					resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
-					resp.flushBuffer();
-					return;
-				}
-
-				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, err);
-				resp.flushBuffer();
-				return;
+				Err e = result.err_msg();
+				resp.sendError(err_to_status(e), e.toString());
+			} else {
+				resp.setStatus(HttpServletResponse.SC_OK);
 			}
-
-			Result<String, String> payload = Serializer.serialize(result.unwrap());
-			if (payload.isErr()) {
-				resp.sendError(
-					HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-					payload.err_msg()
-				);
-				resp.flushBuffer();
-				return;
-			}
-
-			PrintWriter out = resp.getWriter();
-			out.write(payload.unwrap());
-			out.flush();
-			out.close();
-			return;
-		}
-
-
-		String id_param = req.getParameter("id");
-		if (id_param == null) {
-			resp.sendError(
-				HttpServletResponse.SC_BAD_REQUEST,
-				"Must set rollno or pattern"
-			);
 			resp.flushBuffer();
-			return;
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
 		}
-
-		Optional<Long> id = Parser.parse_long(id_param);
-		if (id.isEmpty()) {
-			resp.sendError(
-				HttpServletResponse.SC_BAD_REQUEST,
-				"ID must be a number"
-			);
-			resp.flushBuffer();
-			return;
-		}
-
-		Result<Teacher, String> result = Teacher.search(id.get());
-		if (result.isErr()) {
-			resp.sendError(
-				HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-				result.err_msg()
-			);
-		}
-
-		Result<String, String> payload = Serializer.serialize(result.unwrap());
-		if (payload.isErr()) {
-			resp.sendError(
-				HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-				payload.err_msg()
-			);
-			resp.flushBuffer();
-			return;
-		}
-		PrintWriter out = resp.getWriter();
-		out.write(payload.unwrap());
-		out.flush();
-		out.close();
-		return;
 	}
 }
