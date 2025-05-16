a86d301bd5a19054e8bdfc814ac785576296d676
Dinesh Kumar <gdineshda@gmail.com>
2025-05-16 18:17:03 +0530

    REFACTOR: Use Monadic API

--- app/src/main/java/org/example/api/BatchHelper.java
+++ app/src/main/java/org/example/api/BatchHelper.java
@@ -1,9 +1,13 @@
 package org.example.api;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Optional;
 
 import org.example.data.BatchData;
+import org.example.types.Err;
+import org.example.types.extractors.BatchDataAddRequest;
+import org.example.types.extractors.BatchDataDeleteRequest;
 import org.example.util.Parser;
 import org.example.util.Result;
 
@@ -16,163 +20,72 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = "/api/batch/student")
 public class BatchHelper extends HttpServlet {
 	@Override
-	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String batchid_param = req.getParameter("batchid");
-		if (batchid_param == null) {
-			resp.sendError(
-				HttpServletResponse.SC_BAD_REQUEST,
-				"Must set batchid"
-			);
-			resp.flushBuffer();
-			return;
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			BatchDataAddRequest.extract(req.getParameterMap())
+				.and_then(request -> handle_add(request))
+				.or_else(e -> send_err(resp, e));
+		// this is to catch IOException and ServletException that may appear
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
 		}
-
-		Optional<Long> batchid = Parser.parse_long(batchid_param);
-		if (batchid.isEmpty()) {
-			resp.sendError(
-				HttpServletResponse.SC_BAD_REQUEST,
-				"batchid must be postively numeric"
-			);
-			resp.flushBuffer();
-			return;
-		}
-
-		String rollno_param = req.getParameter("rollno");
-		Optional<Long> rollno = Optional.empty();
-		if (rollno_param != null) {
-			rollno = Parser.parse_long(rollno_param);
-			if (rollno.isEmpty()) {
-				resp.sendError(
-					HttpServletResponse.SC_BAD_REQUEST,
-					"rollno must be set"
-				);
-				resp.flushBuffer();
-				return;
-			}
-		}
-
-		String rollno_group_param = req.getParameter("rollno[]");
-		Optional<long[]> rollno_group = Optional.empty();
-		if (rollno_group_param != null) {
-			rollno_group = Parser.parse_long(rollno_group_param.split(","));
-			if (rollno_group.isEmpty()) {
-				resp.sendError(
-					HttpServletResponse.SC_BAD_REQUEST,
-					"rollno[] is messy"
-				);
-				resp.flushBuffer();
-				return;
-			}
-		}
-
-		if (rollno_group.isEmpty() && rollno.isEmpty()) {
-			resp.sendError(
-				HttpServletResponse.SC_BAD_REQUEST,
-				"must set either rollno[] or rollno"
-			);
-			resp.flushBuffer();
-			return;
-		}
-
-		Result<Void, String> result;
-		if (rollno.isPresent()) {
-			result = BatchData.delete(batchid.get(), rollno.get());
-		} else if (rollno_group.isPresent()) {
-			result = BatchData.delete(batchid.get(), rollno_group.get());
-		} else {
-			result = Result.err("How do you get here?! This is unreachable");
-		}
-
-		if (result.isErr()) {
-			resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, result.err_msg());
-			resp.flushBuffer();
-			return;
-		}
-
-		resp.setStatus(HttpServletResponse.SC_OK);
-		resp.flushBuffer();
-		return;
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String batchid_param = req.getParameter("batchid");
-		if (batchid_param == null) {
-			resp.sendError(
-				HttpServletResponse.SC_BAD_REQUEST,
-				"Must set batchid"
-			);
-			resp.flushBuffer();
-			return;
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			BatchDataDeleteRequest.extract(req.getParameterMap())
+				.and_then(request -> handle_deletion(request))
+				.or_else(e -> send_err(resp, e));
+		// this is to catch IOException and ServletException that may appear
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
 		}
-
-		Optional<Long> batchid = Parser.parse_long(batchid_param);
-		if (batchid.isEmpty()) {
-			resp.sendError(
-				HttpServletResponse.SC_BAD_REQUEST,
-				"batchid must be postively numeric"
-			);
-			resp.flushBuffer();
-			return;
-		}
-
-		String rollno_param = req.getParameter("rollno");
-		Optional<Long> rollno = Optional.empty();
-		if (rollno_param != null) {
-			rollno = Parser.parse_long(rollno_param);
-			if (rollno.isEmpty()) {
-				resp.sendError(
-					HttpServletResponse.SC_BAD_REQUEST,
-					"rollno must be set or rollno[] must be set"
-				);
-				resp.flushBuffer();
-				return;
-			}
-		}
-
-		String rollno_group_param = req.getParameter("rollno[]");
-		Optional<long[]> rollnos = Optional.empty();
-		if (rollno_group_param != null) {
-			rollnos = Parser.parse_long(rollno_group_param.split(","));
-			if (rollnos.isEmpty()) {
-				resp.sendError(
-					HttpServletResponse.SC_BAD_REQUEST,
-					"rollno[] is messy"
-				);
-				resp.flushBuffer();
-				return;
-			}
-		}
-
-		if (rollnos.isEmpty() && rollno.isEmpty()) {
-			resp.sendError(
-				HttpServletResponse.SC_BAD_REQUEST,
-				"must set either rollno[] or rollno"
-			);
-			resp.flushBuffer();
-			return;
-		}
-
-		Result<Void, String> result;
-		if (rollnos.isPresent()) {
-			result = BatchData.add(batchid.get(), rollnos.get());
-		} else if (rollno.isPresent()){
-			result = BatchData.add(batchid.get(), rollno.get());
-		} else {
-			result = Result.err("How did you get here?! It's unreachable");
-		}
-
-		if (result.isErr()) {
-			resp.sendError(
-				HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-				result.err_msg()
-			);
-			resp.flushBuffer();
-			return;
-		}
-
-		resp.setStatus(HttpServletResponse.SC_CREATED);
-		resp.flushBuffer();
-		return;
 	}
+
+	private Result<Void, Err> handle_deletion(BatchDataDeleteRequest request) {
+		if (request.getRollnos() != null) {
+			return BatchData.delete(request.getBatchid().get(), request.getRollnos().get());
+		} else {
+			return BatchData.delete(request.getBatchid().get(), request.getRollno().get());
+		}
+	}
+
+	private static Result<Void, Err> handle_add(BatchDataAddRequest request) {
+		if (request.getRollnos() != null) {
+			return BatchData.add(request.getBatchid().get(), request.getRollnos().get());
+		} else {
+			return BatchData.add(request.getBatchid().get(), request.getRollno().get());
+		}
+	}
+
+	private static<T> Result<T, Void> send_err(HttpServletResponse resp, Err e) {
+		try {
+			resp.sendError(
+				err_to_status(e),
+				e.toString()
+			);
+			resp.flushBuffer();
+			return Result.ok(null);
+		} catch (IOException ex) {
+			return Result.err(null);
+		}
+	}
+
+	private static int err_to_status(Err e) {
+		switch (e.kind) {
+			case ElementNotFound:
+				return HttpServletResponse.SC_NO_CONTENT;
+			case OutOfMemory:
+			case ClassNotFound:
+			case IllegalState:
+			case DBTimeout:
+			case IOError:
+			case JsonSerializeError:
+				return HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+			default:
+				return HttpServletResponse.SC_BAD_REQUEST;
+		}
+	}
+
 }
---
+++ app/src/main/java/org/example/types/extractors/BatchDataAddRequest.java
@@ -0,0 +1,107 @@
+package org.example.types.extractors;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.example.types.Err;
+import org.example.types.ErrKind;
+import org.example.util.Extractor;
+import org.example.util.Parser;
+import org.example.util.Result;
+
+public class BatchDataAddRequest implements Extractor<BatchDataAddRequest> {
+	public static Result<BatchDataAddRequest, Err> extract(Map<String, String[]> map) {
+		if (map == null || map.size() == 1) {
+			return Result.err(new Err(
+				ErrKind.IllegalArgument,
+				"Parameter map is empty"
+			));
+		}
+		BatchDataAddRequest request = new BatchDataAddRequest();
+		String[] batchid = map.get("batchid");
+		String[] rollno = map.get("rollno");
+		String[] rollnos = map.get("rollno[]");
+		if (batchid == null || (rollno == null && rollnos == null)) {
+			return Result.err(new Err(
+				ErrKind.IllegalArgument,
+				"Cannot add to batch without either `batchid` | (`rollno` | `rollnos`)"
+			));
+		}
+		if (batchid != null) {
+			if (batchid.length != 1) {
+				return Result.err(new Err(
+					ErrKind.IllegalArgument,
+					"Parameter 'batchid' must be singular"
+				));
+			}
+			Result<Long, Err> parsed_batchid = Parser.parse_long(batchid[0]);
+			if (parsed_batchid.isErr()) {
+				return Result.err(parsed_batchid.err_msg());
+			}
+
+			request.setBatchid(parsed_batchid.asOption());
+		}
+		if (rollnos != null) {
+			if (rollnos.length != 1) {
+				return Result.err(new Err(
+					ErrKind.IllegalArgument,
+					"Request parameters `rollnos` must be singular"
+				));
+			}
+			// TODO: Better validation of rollnos format
+			Result<long[], Err> result = Parser.parse_long(rollnos[0].split(","));
+			if (result.isErr()) {
+				return Result.err(result.err_msg());
+			}
+			request.setRollnos(result.asOption());
+		} else {
+			if (rollno.length != 1) {
+				return Result.err(new Err(
+					ErrKind.IllegalArgument,
+					"Request parameters `rollno` must be singular"
+				));
+			}
+			Result<Long, Err> result = Parser.parse_long(rollno[0]);
+			if (result.isErr()) {
+				return Result.err(result.err_msg());
+			}
+			request.setRollno(result.asOption());
+		}
+		return Result.ok(request);
+	}
+	private Optional<Long> batchid = Optional.empty();
+	private Optional<Long> rollno = Optional.empty();
+	private Optional<long[]> rollnos = Optional.empty();
+
+	public Optional<long[]> getRollnos() {
+		return rollnos;
+	}
+
+	public void setRollnos(Optional<long[]> rollnos) {
+		this.rollnos = rollnos;
+	}
+
+	public void setRollnos(long[] rollnos) {
+		this.rollnos = Optional.ofNullable(rollnos);
+	}
+	public Optional<Long> getBatchid() {
+		return batchid;
+	}
+	public void setBatchid(Optional<Long> batchid) {
+		this.batchid = batchid;
+	}
+	public void setBatchid(long batchid) {
+		this.batchid = Optional.ofNullable(batchid);
+	}
+
+	public Optional<Long> getRollno() {
+		return rollno;
+	}
+	public void setRollno(Optional<Long> rollno) {
+		this.rollno = rollno;
+	}
+	public void setRollno(long rollno) {
+		this.rollno = Optional.ofNullable(rollno);
+	}
+
+}
