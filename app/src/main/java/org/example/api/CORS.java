ce14747b29ac921d2cc3ee610e729c07316b83e1
Dinesh Kumar <gdineshda@gmail.com>
2025-05-26 18:20:33 +0530

    RENAME to implement CORS, todo: restrict cors

---
+++ app/src/main/java/org/example/api/CORS.java
@@ -0,0 +1,39 @@
+package org.example.api;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebFilter(urlPatterns = "/*")
+public class CORS implements Filter {
+	@Override
+	public void doFilter(
+		ServletRequest request,
+		ServletResponse response,
+		FilterChain chain
+	) throws IOException, ServletException {
+		HttpServletResponse resp = (HttpServletResponse) response;
+
+		resp.setHeader(
+			"Access-Control-Allow-Origin", 
+			"*"
+		);
+		resp.setHeader(
+			"Access-Control-Allow-Methods", 
+			"GET, POST, DELETE"
+		);
+		resp.setHeader(
+			"Access-Control-Allow-Headers", 
+			"*"
+		);
+		System.out.println("I am running a filter");
+		chain.doFilter(request, response);
+	}
+}
