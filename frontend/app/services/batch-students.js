180e3e86fbc852e8d22a6b9d90ca0a82618849d6
Dinesh Kumar <gdineshda@gmail.com>
2025-05-29 15:51:58 +0530

    Experiment with Non Duplicate Stuff

--- frontend/app/services/batch-students.js
+++ frontend/app/services/batch-students.js
@@ -8,6 +8,20 @@ export default class BatchStudentsService extends Service {
     return this.elements;
   }
 
+  removeDuplicates() {
+    let i = 0;
+    let length = this.elements.length;
+    let filtered = this.elements
+      .sort()
+      .filter((val, idx, arr) => {
+        if (idx < length - 1) {
+          arr.at(idx + 1) != val;
+        }
+      });
+
+    this.elements = TrackedArray.from(filtered);
+  }
+
   add(element) {
     if (element) {
       this.elements.push(element);
